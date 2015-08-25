5235b1f4a5002855579b3600777463fcc823f6f5
John Huynh <john@huynh.io>
2015-08-25 00:23:19 -0400

    Add newlines and spaces so the code can breath.

--- addon/index.js
+++ addon/index.js
@@ -1,28 +1,33 @@
 import Ember from 'ember';
 
 var extendedComponent = false;
+
 if (!extendedComponent) {
   Ember.Component.reopen({
     checkStrongAttrs: Ember.on('init', function(){
       const declaredStrongAttrs = this.constructor.superclass.declaredStrongAttrs;
+
       if (!declaredStrongAttrs) { return; }
+
       declaredStrongAttrs.forEach((declaredAttr) => {
         const val = this.getAttr(declaredAttr.name);
         const isUndefined = val === undefined;
+
         if (isUndefined && declaredAttr.required) {
           throw new Error(`Component ${this.toString()} missing required attribute '${declaredAttr.name}'. Expected type '${typeToString(declaredAttr.type)}'`)
         }
+
         if (declaredAttr.type && !isUndefined) {
           validateType(declaredAttr, val, this);
         }
       });
     })
   });
+
   extendedComponent = true;
 }
 
-
-export function requiredAttr(attrName, attrType){
+export function requiredAttr(attrName, attrType) {
   return function(target) {
     declareAttr(target, attrName, attrType, true);
   };
@@ -43,6 +48,7 @@ function declareAttr(target, attrName, attrType, isRequired) {
       value: []
     });
   }
+
   target.declaredStrongAttrs.push({
     name: attrName,
     type: attrType,
